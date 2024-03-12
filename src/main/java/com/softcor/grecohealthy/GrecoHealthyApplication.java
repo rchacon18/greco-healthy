8e8567fea05a859f40adb83540927b00352f2c1f
rchacon18 <richi_4515@hotmail.com>
2024-03-12 15:34:52 -0600

    #GH-3 Maven project generated

---
+++ src/main/java/com/softcor/grecohealthy/GrecoHealthyApplication.java
@@ -0,0 +1,13 @@
+package com.softcor.grecohealthy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrecoHealthyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrecoHealthyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softcor/grecohealthy/GrecoHealthyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softcor.grecohealthy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrecoHealthyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
